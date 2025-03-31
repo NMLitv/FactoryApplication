93d73e6423ecd4aa4612df6ad8feed9f0e9cd2e5
Nikita Litvinov <litvinovnikita97@mail.ru>
2025-03-31 23:30:44 +0300

    first

---
+++ src/test/java/firstproject/factoryapplication/FactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package firstproject.factoryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
