aa38532d5a28dc9db24a1229fa0bfe022d07bd24
Nikita Litvinov <litvinovnikita97@mail.ru>
2025-05-01 18:34:10 +0400

    связал такси и оборудование
    Signed-off-by: Nikita Litvinov <litvinovnikita97@mail.ru>

---
+++ src/main/java/firstproject/factoryapplication/repository/EquipmentRepository.java
@@ -0,0 +1,15 @@
+package firstproject.factoryapplication.repository;
+
+import firstproject.factoryapplication.model.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+
+    List<Equipment> findByStatus(String status);
+
+    List<Equipment> findByType(String type);
+}
